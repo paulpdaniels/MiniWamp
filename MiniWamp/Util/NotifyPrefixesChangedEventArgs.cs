b43bc6123173d8c5288e112859208aec24fa0823
paulpdaniels <paulpdaniels@gmail.com>
2015-08-10 15:32:31 -0700

    Added NotifyPrefixesChangedEventArgs

---
+++ MiniWamp/Util/NotifyPrefixesChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace DapperWare.Util
+{
+    public class NotifyPrefixesChangedEventArgs : EventArgs
+    {
+        public NotifyCollectionChangedAction Action { get; private set; }
+
+        public KeyValuePair<string, string> Prefix { get; private set; }
+
+        public NotifyPrefixesChangedEventArgs(NotifyCollectionChangedAction action, KeyValuePair<string, string> prefix)
+        {
+            Action = action;
+            Prefix = prefix;
+        }
+    }
+}
