47cc783e24b5102734a63972a41e17f3ac30ff6b
paulpdaniels <paulpdaniels@gmail.com>
2015-10-17 12:12:49 -0700

    Extracted test project

---
+++ MiniWampTests.Net45/JsonSerializerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Testing
+#if !NETFX_CORE
+
+namespace DapperWare.Serialization
+{
+    using NUnit.Framework;
+    using System.IO;
+
+    [TestFixture]
+    public class TestJsonSerializer
+    {
+
+        private ISerializer serializer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            serializer = new JsonSerializer();
+        }
+
+
+        [Test]
+        public void TestSingleValueArray()
+        {
+            using (var ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, new object[] { 5 });
+
+                var result = Encoding.UTF8.GetString(ms.ToArray());
+
+                Assert.AreEqual(result, "[5]");
+            }
+        }
+
+        [Test]
+        public void TestEmptyArray()
+        {
+            using (var ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, new object[] { });
+                var result = Encoding.UTF8.GetString(ms.ToArray());
+                Assert.AreEqual(result, "[]");
+            }
+        }
+
+        [Test]
+        public void TestVariantTypeArray()
+        {
+            using (var ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, new object[] { 5, "blah", true });
+                var result = Encoding.UTF8.GetString(ms.ToArray());
+                Assert.AreEqual(result, "[5,\"blah\",true]");
+            }
+        }
+
+    }
+}
+
+#endif
+#endregion
---
+++ MiniWampTests.Net45/WampSubscriptionTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Testing
+#if !NETFX_CORE
+
+namespace DapperWare
+{
+
+    using NUnit.Framework;
+    using Moq;
+    using Newtonsoft.Json.Linq;
+
+    [TestFixture]
+    public class WampSubscriptionTests
+    {
+
+        Mock<IWampSession> mockSession;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockSession = new Mock<IWampSession>();
+        }
+
+        [Test]
+        public void TestRaiseEvent()
+        {
+            var subscription = new WampSubscription<int>(mockSession.Object,
+                "http://faketopic.com/something#resource");
+
+            var mockEventHandler = new Mock<EventHandler<WampSubscriptionMessageEventArgs<int>>>();
+
+            subscription.Event += mockEventHandler.Object;
+
+            subscription.HandleEvent(new JValue(5));
+
+            mockEventHandler.Verify(eh => eh(subscription,
+                It.Is<WampSubscriptionMessageEventArgs<int>>(left => left.Value == 5)), Times.Once());
+        }
+
+        [Test]
+        public void TestUnsubscribeSession()
+        {
+            var subscription = new WampSubscription<int>(mockSession.Object,
+                "http://faketopic.com/something#resource");
+
+            subscription.Dispose();
+
+            mockSession.Verify(sess => sess.Unsubscribe("http://faketopic.com/something#resource"));
+        }
+
+        [Test]
+        public void TestUnsubscribeSessionWithExistingSubject()
+        {
+            var subscription = new WampSubscription<int>(mockSession.Object,
+                "http://faketopic.com/something#resource");
+
+            var subject = subscription.CreateSubject();
+
+            subscription.Dispose();
+
+            mockSession.Verify(sess => sess.Unsubscribe("http://faketopic.com/something#resource"));
+
+        }
+
+    }
+
+}
+
+#endif
+
+#endregion
