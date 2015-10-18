c9c83c9496b1b032a454d1c88509e3c0a154b7e6
paulpdaniels <paulpdaniels@gmail.com>
2015-10-18 12:02:17 -0700

    Fixing the build again.

--- build.bat
+++ build.bat
@@ -9,9 +9,14 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
+REM Package restore
+call %NuGet% restore MiniWamp\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore MiniWampTests.Net45\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore MiniWampTests\packages.config -OutputDirectory %cd%\packages -NonInteractive
+
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild MiniWamp.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild YourSolution.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir Build
-call %nuget% pack "MiniWamp.nuspec" -o Build -p Configuration=%config% %version%
+call %nuget% pack "MiniWamp.nuspec" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
